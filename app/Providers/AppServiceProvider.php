74e09f6b4cac7ba3f3c8ff3259a036a4af3b5630
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2025-02-12 21:10:54 +0800

    Force HTTPS scheme and set Carbon locale based on application locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $url = parse_url(url('/'));
+        if($url['scheme'] == 'https'){
+            URL::forceScheme('https');
+        }
+        Carbon::setLocale(LC_TIME, $this->app->getLocale());
     }
 }
